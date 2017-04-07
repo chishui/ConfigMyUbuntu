7a9868cfb74efcfc6ac88f50935fdd4c83faf25e
chishui <chishui2@gmail.com>
2017-04-07 15:09:19 -0700

    initial commit

---
+++ run.sh
@@ -0,0 +1,30 @@
+sudo apt-get update
+sudo apt-get -y install build-essential tmux curl vim git autojump zsh python python-dev python-setuptools cmake
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | bash
+mkdir ~/projects
+cd projects
+git clone https://github.com/chishui/dotfile.git
+cp ~/projects/dotfile/.vimrc ~
+cp ~/projects/dotfile/.zshrc ~
+cp ~/projects/dotfile/robbyrussell.zsh-theme /root/.oh-my-zsh/themes
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/vim-colorschemes
+vim +PluginInstall  +qall
+cd /root/.vim/bundle/YouCompleteMe/
+YCM_CORES=1 python ./install.py --clang-completer
+
+#theme
+sudo apt-get install unity-tweak-tool
+
+sudo add-apt-repository ppa:snwh/pulp
+# update repository info
+sudo apt-get update
+# install icon theme
+sudo apt-get install paper-icon-theme
+# install gtk theme
+sudo apt-get install paper-gtk-theme
+# install cursor theme
+sudo apt-get install paper-cursor-theme
+
+
+
